331a63b048783a070359776817047ade2c2504b9
Globik <globalikslivov@gmail.com>
2025-12-11 23:54:05 +0500

    a

--- public/js/webrtc8.js
+++ public/js/webrtc8.js
@@ -3156,14 +3156,14 @@ async  function createPeerConnection () {
 		"urls":[
 		//"stun:127.0.0.1:3478",
 		"stun:stun.l.google.com:19302",
-		"stun:rouletk.ru:3479"
+		"stun:chatikon.ru:3479"
 		]
 		//stun:45.12.18.172:3479
 		},
 	{
 		urls:[
 	//"turn:127.0.1:3478",
-		"turn:ruletka.ru:3479?transport=udp", 
+		"turn:chatikon.ru:3479?transport=udp", 
 		//"turn:5.35.88.151:3479?transport=tcp", 
 		//"turn:rouletka.ru:5348",
 		//"turn:rouletka.ru:5348?transport=tcp" ,
