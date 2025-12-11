f8cd5645d43b88285bae4844b566a44925c1a36e
Globik <globalikslivov@gmail.com>
2025-12-12 00:50:57 +0500

    a

--- public/js/webrtc8.js
+++ public/js/webrtc8.js
@@ -2917,7 +2917,7 @@ window.addEventListener("online", function(e) {
 			 setTimeout(function(){
 			  let imgdata3 = Screenshot();
 			  //let lala = IPS
-			//  wsend( { type:'search-peer', nick: (NICK?NICK:"Anoni"), src: imgdata3, ignores: [...IPS]});
+		 wsend( { type:'search-peer', nick: (NICK?NICK:"Anoni"), src: imgdata3, ignores: [...IPS]});
 		  }, 0);
 		 }else{
 			 console.warn("CONNECTED");
