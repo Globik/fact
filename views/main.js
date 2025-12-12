8f46620ae93c1e727021984383b1bce225a14bd5
Globik <globalikslivov@gmail.com>
2025-12-12 14:33:18 +0500

    a

--- views/main.js
+++ views/main.js
@@ -668,7 +668,8 @@ lang=='id'?'tentang kami':''}</a> -->
 <b style="display:inline-block;margin:3px;line-height:1.3;padding-bottom:1px;transform:translateY(-2px);color:blue;font-size:0.9rem;">Share</b></div>
 <div style="margin-left:4px;"><script>document.write(VK.Share.button(false,{type:"round",text:"Share"}));</script></div>
 -->
-<div class="ya-share2" data-curtain data-size="m" data-shape="round"  data-services="vkontakte,telegram,odnoklassniki" data-url="https://chatikon.ru" data-image="https://chatikon.ru/og_image.png"></div>
+<!-- <div class="ya-share2" data-curtain data-size="m" data-shape="round"  data-services="vkontakte,telegram,odnoklassniki" 
+data-url="https://chatikon.ru" data-image="https://chatikon.ru/og_image.png"></div> -->
 </div></div>
 ${n.VK?`<div id="chatruleslink"><a href="#regeln">Правила чата</a></div>`:''}
 <script>
