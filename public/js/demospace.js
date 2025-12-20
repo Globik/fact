b2b935ab30403c2da8e011896581fd067fa523b9
Globik <globalikslivov@gmail.com>
2025-12-21 02:05:52 +0500

    a

--- public/js/demospace.js
+++ public/js/demospace.js
@@ -181,7 +181,7 @@ function getJanus(){
 Janus.init({debug: "all", callback: function() {
 	janus = new Janus(
 				{
-					server: "ws://"+ window.location.hostname + ":8188/janus";,
+					server: "ws://"+ window.location.hostname + ":8188/janus",
 					iceServers: null,
 					// Should the Janus API require authentication, you can specify either the API secret or user token here too
 					//		token: "mytoken",
