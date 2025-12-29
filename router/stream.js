c27d89f9fce3530bc271ca082555fdf76db866b2
Globik <globalikslivov@gmail.com>
2025-12-29 06:48:29 +0500

    a

--- router/stream.js
+++ router/stream.js
@@ -3,6 +3,19 @@ const axios = require('axios').default;
 const router = express.Router();
 const app = express()
 const VIDEOCHAT_TG_ID = '-1002494074502';
+async function botMessage(txt){
+	if(process.env.DEVELOPMENT == 'yes')return;
+	try{
+		await axios.post(`https://api.telegram.org/bot${tg_api}/sendMessage`, {
+    chat_id: VIDEOCHAT_TG_ID,
+    text: txt,
+    parse_mode: 'html',
+    disable_notification: false
+  });
+	}catch(e){
+		console.log(e);
+		}
+}
 
 app.get('/:id/:streamid', async(req, res)=>{
 	//console.log('params ', req.params);
@@ -26,19 +39,7 @@ app.get('/:id', async(req, res)=>{
 		}
 	}
 	console.log('user ', req.user);
+	botMessage('wanna stream');
 	res.rendel('stream',{ owner:owner, lang: 'ru' , userid: req.params.id, user:req.user });
 })
 module.exports = app;
-async function botMessage(txt){
-	if(process.env.DEVELOPMENT == 'yes')return;
-	try{
-		await axios.post(`https://api.telegram.org/bot${tg_api}/sendMessage`, {
-    chat_id: VIDEOCHAT_TG_ID,
-    text: txt,
-    parse_mode: 'html',
-    disable_notification: false
-  });
-	}catch(e){
-		console.log(e);
-		}
-}
