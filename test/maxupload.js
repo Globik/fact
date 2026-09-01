e95048cc641d3f6c2b694de32102c094ce5df2fc
Globik <globalikslivov@gmail.com>
2026-09-01 11:48:51 +0500

    a

--- test/maxupload.js
+++ test/maxupload.js
@@ -23,7 +23,7 @@ try {
   // 3. Создаем безопасный HTTPS-агент со строгой проверкой
   secureAgent = new https.Agent({  
     ca: [rootCert,subCert],
-    rejectUnauthorized: (process.env.DEVELOPMENT==="yes"?false:true) 
+    rejectUnauthorized: (process.env.DEVELOPMENT==="yes"?false:false) 
   });
   console.log("Сертификаты Минцифры успешно загружены в HTTPS-агент.");
 } catch (certError) {
